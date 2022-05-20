b06e1a10528c036bf061aa81332516b9de2afa9d
elizabethhwang <82409001+elizabethhwang@users.noreply.github.com>
2022-05-19 17:58:52 -0700

    Add files via upload

---
+++ Test_hand_openclose.ino
@@ -0,0 +1,42 @@
+#include <Servo.h>
+
+Servo myservo1;
+Servo myservo2;
+Servo myservo3;
+Servo myservo4;
+Servo myservo5;
+
+int pos1 = 0;
+int pos2 = 180;
+
+void setup() {
+  // put your setup code here, to run once:
+  
+  myservo1.attach(15);
+  myservo2.attach(16);
+  myservo3.attach(17);
+  myservo4.attach(18);
+  myservo5.attach(19);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  myservo1.write(pos1);
+  myservo2.write(pos1);
+  myservo3.write(pos1);
+  myservo4.write(pos1);
+  myservo5.write(pos1);
+
+
+  delay(2000);
+
+  myservo1.write(pos2);
+  myservo2.write(pos2);
+  myservo3.write(pos2);
+  myservo4.write(pos2);
+  myservo5.write(pos2);
+
+  delay(2000);
+}
